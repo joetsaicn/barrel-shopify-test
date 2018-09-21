0d5edf9f55b99e4db56cac061dade6d52cc75c51
Max Rolon <max.rolon@barrelny.com>
2018-09-21 13:17:17 -0400

    Updates init fn of account-forms

--- src/modules/account-forms/account-forms.js
+++ src/modules/account-forms/account-forms.js
@@ -1,40 +1,31 @@
 import Vue from 'vue'
 import qs from 'query-string'
 
-const AccountForms = {
-  data () {
-    return {
-      activeForm: 'login'
-    }
-  },
-  mounted () {
-    this.changeForm()
-  },
-  methods: {
-    changeForm (form = false) {
-      const parsed = qs.parse(location.search)
-      if (!form && !parsed.form) {
-        return
+export default el => {
+  return new Vue({
+    el,
+    data () {
+      return {
+        activeForm: 'login'
       }
-      if (!form) {
-        form = parsed.form
-      } else {
-        parsed.form = form
+    },
+    mounted () {
+      this.changeForm()
+    },
+    methods: {
+      changeForm (form = false) {
+        const parsed = qs.parse(location.search)
+        if (!form && !parsed.form) {
+          return
+        }
+        if (!form) {
+          form = parsed.form
+        } else {
+          parsed.form = form
+        }
+        this.activeForm = form
+        history.replaceState(null, null, `${location.pathname}?${qs.stringify(parsed)}`)
       }
-      this.activeForm = form
-      history.replaceState(null, null, `${location.pathname}?${qs.stringify(parsed)}`)
     }
-  }
-}
-
-export default {
-  init (el) {
-    this.instance = new Vue({
-      el,
-      ...AccountForms
-    })
-  },
-  destroy () {
-    this.instance.$destroy()
-  }
+  })
 }
