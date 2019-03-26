c0d56489e46d3279c0d3328af9f94bfe33a32f0d
Max <max.rolon@barrelny.com>
2019-03-26 10:51:05 -0400

    Ports over base modules and templates to base-modules repo

--- src/modules/hero/hero.js
+++
@@ -1,3 +0,0 @@
-module.exports = (el) => {
-  console.dir(`Module: hero`)
-}
--- src/modules/promobar/promobar.js
+++
@@ -1,9 +0,0 @@
-import promobar from 'promobar'
-
-export default el => {
-  const promo = promobar(el)
-
-  setTimeout(promo.show, 4000)
-
-  window.promobar = promo
-}
