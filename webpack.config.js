6d0dba0b892440f7ace1ee056c00d9ee40355900
Wes Turner <wturnerharris@gmail.com>
2020-02-17 12:45:40 -0500

    Update package config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: {
-    main: ['./src/assets/css/main.css', './src/assets/js/main']
+    main: ['./src/assets/css/main.css','./src/assets/js/main'],
   },
   output: {
     path: path.join(__dirname, 'dist/assets'),
@@ -13,10 +13,10 @@ module.exports = {
   module: {
     rules: [
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader'
+        loader: "eslint-loader",
       },
       {
         test: /\.js$/,
@@ -32,17 +32,17 @@ module.exports = {
           'css-loader?importLoaders=1',
           'postcss-loader'
         ]
-      }
+      },
     ]
   },
   resolve: {
     alias: {
       'lib': path.resolve(__dirname, 'src/assets/js/lib'),
       'modules': path.resolve(__dirname, 'src/modules'),
-      'mixins': path.resolve(__dirname, 'src/assets/js/mixins'),
       'root': path.resolve(__dirname, 'src'),
-      'vue': process.env.ENV === 'production' ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'
-    }
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    },
   },
-  plugins: []
+  plugins: [],
 }
