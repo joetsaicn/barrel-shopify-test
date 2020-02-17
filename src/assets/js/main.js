e31363596286f0e1c864d634e6fa1009c4b0f493
Wes Turner <wturnerharris@gmail.com>
2020-02-17 12:42:20 -0500

    Update test with missing code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,18 @@
-import app from 'lib/init'
-__webpack_public_path__ = BRRL_PATH(BRRL_PUBLIC_PATH) // eslint-disable-line
+__webpack_public_path__ = BRRL_PATH(BRRL_PUBLIC_PATH) // eslint-disable-line camelcase
+
+import 'lazysizes/plugins/object-fit/ls.object-fit'
+import 'lazysizes/plugins/parent-fit/ls.parent-fit'
+import 'lazysizes/plugins/rias/ls.rias'
+import 'lazysizes/plugins/bgset/ls.bgset'
+import 'lazysizes'
+import 'lazysizes/plugins/respimg/ls.respimg'
+
+import init from 'lib/init'
+import { set, unset, toggle, isMobile } from 'lib/util'
 
 document.addEventListener('DOMContentLoaded', () => {
-  app.init()
+  document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js')
+  init({
+    module: 'modules'
+  }).mount()
 })
