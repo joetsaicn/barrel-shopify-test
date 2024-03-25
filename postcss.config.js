ba805c6eb6df3e86a939387134e5f5ad94716f07
Hugh Bui <hugh.bui@barreldev.com>
2024-03-25 15:26:11 +0700

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,13 @@
-const config = {
+module.exports = {
   plugins: [
-    require('./postcss-tasks/postcss-module-import'),
+    require('precss'),
+    require('postcss-mixins'),
     require('postcss-inline-svg'),
     require("postcss-color-function"),
-    require('autoprefixer')({
-      browsers: [
-        'last 3 versions',
-        'iOS >= 8',
-        'Safari >= 8',
-        'ie 11',
-      ]
-    }),
+    require('autoprefixer'),
+    require('postcss-automath'),
+    require('postcss-hexrgba'),
     require('postcss-extend'),
+    require('postcss-each')
   ]
 }
-
-if (process.env.ENV === 'development') {
-  // Chuck in directly after importing files
-  config.plugins.splice(1, 0,
-    require('./postcss-tasks/postcss-shopify-fonts')('//cdn.shopify.com/s/files/<add-cdn-uri-here>/assets/')
-  )
-}
-
-config.plugins.push(require('precss'))
-
-module.exports = config
